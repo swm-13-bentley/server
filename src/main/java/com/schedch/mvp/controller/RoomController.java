c012206001ce39313cebbbeec023c389932fb051
hwan <hshlego@naver.com>
2022-07-11 17:16:56 +0900

    test : Add RoomController added, integrated test not done.

--- src/main/java/com/schedch/mvp/controller/RoomController.java
+++ src/main/java/com/schedch/mvp/controller/RoomController.java
@@ -1,9 +1,19 @@
 package com.schedch.mvp.controller;
 
+import com.schedch.mvp.dto.RoomRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class RoomController {
 
-
+    @PostMapping("/room")
+    public String createRoom(@RequestBody RoomRequestDto roomRequestDto) {
+        return "success";
+    }
 }
