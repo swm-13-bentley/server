2b0ce67c3405838f32e3da70fe38de7feeeda36c
hwan <hshlego@naver.com>
2022-07-11 18:12:53 +0900

    feat : Add crateRoom() added.

--- src/main/java/com/schedch/mvp/service/RoomService.java
+++ src/main/java/com/schedch/mvp/service/RoomService.java
@@ -1,10 +1,21 @@
 package com.schedch.mvp.service;
 
+import com.schedch.mvp.dto.RoomRequestDto;
+import com.schedch.mvp.model.Room;
+import com.schedch.mvp.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class RoomService {
 
+    private final RoomRepository roomRepository;
+    public String createRoom(RoomRequestDto roomRequestDto) {
+        Room room = new Room(roomRequestDto);
+        Room save = roomRepository.save(room);
+        return save.getUuid();
+    }
 }
