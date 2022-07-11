d4a3b5cec0949f5ff7de6b9a85241c67bb31fbcd
hwan <hshlego@naver.com>
2022-07-11 17:41:13 +0900

    feat : Add RoomRepository added

---
+++ src/main/java/com/schedch/mvp/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.schedch.mvp.repository;
+
+import com.schedch.mvp.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
