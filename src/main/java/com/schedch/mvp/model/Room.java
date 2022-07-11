3d43a8b0be654bae635e218799cb9020419705cf
hwan <hshlego@naver.com>
2022-07-11 17:38:02 +0900

    feat : Add dto -> entity constructor added

--- src/main/java/com/schedch/mvp/model/Room.java
+++ src/main/java/com/schedch/mvp/model/Room.java
@@ -1,15 +1,18 @@
 package com.schedch.mvp.model;
 
+import com.schedch.mvp.dto.RoomRequestDto;
 import com.sun.istack.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
@@ -33,6 +36,12 @@ public class Room {
     @NotNull
     private LocalTime endTime;
 
+    //연관관계 편의 메서드
+    public void addRoomDate(RoomDate roomDate) {
+        roomDates.add(roomDate);
+        roomDate.setRoom(this);
+    }
+
     @Builder
     public Room(String title, List<RoomDate> roomDates, LocalTime startTime, LocalTime endTime) {
         this.uuid = UUID.randomUUID().toString();
@@ -40,5 +49,16 @@ public class Room {
         this.roomDates = roomDates;
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public Room(RoomRequestDto roomRequestDto) {
+        this.uuid = UUID.randomUUID().toString();
+        this.title = roomRequestDto.getTitle();
+        roomRequestDto.getDates().stream()
+                .map(localDate -> new RoomDate(localDate))
+                .forEach(roomDate -> addRoomDate(roomDate));
+        this.startTime = roomRequestDto.getStartTime();
+        this.endTime = roomRequestDto.getEndTime();
+
     }
 }
