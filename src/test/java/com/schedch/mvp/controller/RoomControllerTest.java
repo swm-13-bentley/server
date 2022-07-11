4b693252f7cc9b3434b9178d42b172c59ed11992
hwan <hshlego@naver.com>
2022-07-11 16:58:20 +0900

    테스트 코드: post: /room 작성 완료

--- src/test/java/com/schedch/mvp/controller/RoomControllerTest.java
+++ src/test/java/com/schedch/mvp/controller/RoomControllerTest.java
@@ -1,5 +1,11 @@
 package com.schedch.mvp.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.schedch.mvp.dto.RoomRequestDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,6 +16,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,18 +29,28 @@ class RoomControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void room_post_test() throws Exception {
         //given
         String title = "test title";
         LocalDate date1 = LocalDate.of(2022, 04, 01);
         LocalDate date2 = LocalDate.of(2022, 04, 02);
+        List<LocalDate> dates = Arrays.asList(date1, date2);
         LocalTime startTime = LocalTime.of(04, 30, 00);
         LocalTime endTime = LocalTime.of(20, 00, 00);
 
+        RoomRequestDto roomRequestDto = RoomRequestDto.builder()
+                .title(title)
+                .dates(dates)
+                .startTime(startTime)
+                .endTime(endTime)
+                .build();
+
         mockMvc.perform(post("/room")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(title)
+                .content(roomRequestDto.toString())
             )
                 .andExpect(status().isOk());
     }
