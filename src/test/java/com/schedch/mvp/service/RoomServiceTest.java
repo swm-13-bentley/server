71f60f699c2200b2e2f02287f032b48540f3d313
hwan <hshlego@naver.com>
2022-07-11 18:12:24 +0900

    test : Add createRoomTest() complete.

--- src/test/java/com/schedch/mvp/service/RoomServiceTest.java
+++ src/test/java/com/schedch/mvp/service/RoomServiceTest.java
@@ -1,24 +1,56 @@
 package com.schedch.mvp.service;
 
 import com.schedch.mvp.dto.RoomRequestDto;
+import com.schedch.mvp.model.Room;
+import com.schedch.mvp.repository.RoomRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 class RoomServiceTest {
 
-    @Autowired RoomService roomService;
+    private static RoomRepository roomRepository;
+
+    @BeforeAll
+    static void setUp() {
+        roomRepository = Mockito.mock(RoomRepository.class);
+    }
 
     @Test
-    public void roomService_createRoom() throws Exception {
+    public void roomRepository_not_null_test() {
+        assertThat(roomRepository).isNotNull();
+    }
+
+    @Test
+    public void createRoomTest() throws Exception {
         //given
+        RoomRequestDto roomRequestDto = createRoomRequestDto();
+        Room room = new Room(roomRequestDto);
+        when(roomRepository.save(any(Room.class))).thenReturn(new Room(roomRequestDto));
+
+        //when
+        Room savedRoom = roomRepository.save(room);
+
+        //then
+        assertThat(savedRoom.getUuid()).isNotNull();
+
+    }
+
+    private RoomRequestDto createRoomRequestDto() {
         String title = "test title";
         LocalDate date1 = LocalDate.of(2022, 04, 01);
         LocalDate date2 = LocalDate.of(2022, 04, 02);
@@ -32,12 +64,7 @@ class RoomServiceTest {
                 .startTime(startTime)
                 .endTime(endTime)
                 .build();
-
-        //when
-        String roomUuid = roomService.save(roomRequestDto);
-
-        //then
-        assertThat(roomUuid).isNotNull();
-
+        return roomRequestDto;
     }
+
 }
