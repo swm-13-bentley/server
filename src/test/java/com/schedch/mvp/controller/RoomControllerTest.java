e707974ad8ba5e239b95b29780d1ab044babba4d
hwan <hshlego@naver.com>
2022-07-11 16:19:33 +0900

    테스트 코드: RoomController - post("/room")

--- src/test/java/com/schedch/mvp/controller/RoomControllerTest.java
+++ src/test/java/com/schedch/mvp/controller/RoomControllerTest.java
@@ -1,18 +1,38 @@
 package com.schedch.mvp.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-@WebMvcTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RoomController.class)
 class RoomControllerTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @MockBean
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void room_post_test() throws Exception {
+        //given
+        String title = "test title";
+        LocalDate date1 = LocalDate.of(2022, 04, 01);
+        LocalDate date2 = LocalDate.of(2022, 04, 02);
+        LocalTime startTime = LocalTime.of(04, 30, 00);
+        LocalTime endTime = LocalTime.of(20, 00, 00);
+
+        mockMvc.perform(post("/room")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(title)
+            )
+                .andExpect(status().isOk());
+    }
 }
