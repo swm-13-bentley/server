a032bf480c843131e04423b3253342039e01fc08
hwan <hshlego@naver.com>
2022-07-12 11:43:27 +0900

    feat : add ControllerHandler for controller validation added

---
+++ src/main/java/com/schedch/mvp/controller/handler/ControllerHandler.java
@@ -0,0 +1,24 @@
+package com.schedch.mvp.controller.handler;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("message", errorMessage);
+        return new ResponseEntity<>(new Gson().toJson(jsonObject), HttpStatus.BAD_REQUEST);
+    }
+}
