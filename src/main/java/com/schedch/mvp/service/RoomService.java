1852895710f326c107324fdd0fb1258e680c01b6
hwan <hshlego@naver.com>
2022-07-12 15:35:29 +0900

    feat : add getRoomInfo(String roomUuid) created

--- src/main/java/com/schedch/mvp/service/RoomService.java
+++ src/main/java/com/schedch/mvp/service/RoomService.java
@@ -1,11 +1,15 @@
 package com.schedch.mvp.service;
 
 import com.schedch.mvp.dto.RoomRequestDto;
+import com.schedch.mvp.dto.RoomResponseDto;
 import com.schedch.mvp.model.Room;
 import com.schedch.mvp.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -17,5 +21,13 @@ public class RoomService {
         Room room = new Room(roomRequestDto);
         Room save = roomRepository.save(room);
         return save.getUuid();
+    }
+
+    public RoomResponseDto getRoomInfo(String roomUuid) {
+        Optional<Room> roomOptional = roomRepository.findByUuid(roomUuid);
+        Room room = roomOptional.orElseThrow(
+                () -> new NoSuchElementException(String.format("Room for uuid: %s not found", roomUuid))
+            );
+        return new RoomResponseDto(room);
     }
 }
