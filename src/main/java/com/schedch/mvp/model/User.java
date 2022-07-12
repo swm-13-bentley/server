b4e27fbc0ccf3c5366f802ea3399ec99185bcaf7
hwan <hshlego@naver.com>
2022-07-12 20:56:33 +0900

    feat : Add 1. user - participant @OneToMany 매핑
    2. 연관관계 편의 메서드 생성

--- src/main/java/com/schedch/mvp/model/User.java
+++ src/main/java/com/schedch/mvp/model/User.java
@@ -1,5 +1,38 @@
 package com.schedch.mvp.model;
 
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
 public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    private String password;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Participant> participantList = new ArrayList<>();
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Boolean checkPassword(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+
+    public void addParticipant(Participant participant) {
+        participantList.add(participant);
+        participant.setUser(this);
+    }
 }
