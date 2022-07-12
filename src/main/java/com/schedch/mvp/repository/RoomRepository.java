68da6b32a279809b9cc1dde278d26b53fde02858
hwan <hshlego@naver.com>
2022-07-12 12:41:43 +0900

    feat : add findByUuid added

--- src/main/java/com/schedch/mvp/repository/RoomRepository.java
+++ src/main/java/com/schedch/mvp/repository/RoomRepository.java
@@ -4,6 +4,9 @@ import com.schedch.mvp.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByUuid(String roomUuid);
 }
