5c91d77c0d0693ba718654d194a4b4087d73cc90
hwan <hshlego@naver.com>
2022-07-13 15:13:54 +0900

    feat : create ParticipantResponseDto created

---
+++ src/main/java/com/schedch/mvp/dto/ParticipantResponseDto.java
@@ -0,0 +1,22 @@
+package com.schedch.mvp.dto;
+
+import com.schedch.mvp.model.Participant;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class ParticipantResponseDto {
+
+    private String participantName;
+    private List<TimeBlockDto> timeBlockDtoList = new ArrayList<>();
+
+    public ParticipantResponseDto(Participant participant) {
+        this.participantName = participant.getParticipantName();
+        this.timeBlockDtoList = participant.getScheduleList().stream()
+                .map(schedule -> schedule.toTimeBlockDto(30))
+                .collect(Collectors.toList());
+    }
+}
