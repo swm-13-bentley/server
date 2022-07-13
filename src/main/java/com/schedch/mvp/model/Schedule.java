f7287d2f4eb7c8af103a51909c42a0d271e78464
hwan <hshlego@naver.com>
2022-07-13 11:00:57 +0900

    feat : add field name changed to availableDate timeBlock 관련 로직 추가

---
+++ src/main/java/com/schedch/mvp/model/Schedule.java
@@ -0,0 +1,61 @@
+package com.schedch.mvp.model;
+
+import com.schedch.mvp.dto.TimeBlockDto;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_id")
+    private Participant participant;
+
+    @NotNull(message = "Schedule scheduledDate cannot be empty")
+    private LocalDate availableDate;
+
+    @NotNull(message = "Schedule startTime cannot be empty")
+    private LocalTime startTime;
+
+    @NotNull(message = "Schedule endTime cannot be empty")
+    private LocalTime endTime;
+
+
+    public Schedule(LocalDate availableDate, LocalTime startTime, LocalTime endTime) {
+        this.availableDate = availableDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public TimeBlockDto toTimeBlockDto(int unit) {
+        return TimeBlockDto.builder()
+                .availableDate(this.availableDate)
+                .availableTimeList(cutTime(unit))
+                .build();
+
+    }
+
+    public List<Integer> cutTime(int unit) {
+        int start = (int) (startTime.getHour() * (60/unit)
+                        + Math.floor(startTime.getMinute() / unit));
+
+        int end = (int) (endTime.getHour() * (60/unit)
+                + Math.floor(endTime.getMinute() / unit));
+
+        return IntStream.range(start, end+1).boxed().collect(Collectors.toList());
+    }
+}
