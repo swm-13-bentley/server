749ca918f442a637320744ddeaa252603f8dd903
hwan <hshlego@naver.com>
2022-07-13 11:02:13 +0900

    chore : modify removed unused import

--- src/test/java/com/schedch/mvp/controller/RoomControllerTest.java
+++ src/test/java/com/schedch/mvp/controller/RoomControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
