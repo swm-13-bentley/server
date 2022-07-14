53d6857133e442e8026fa36e51ffb7ae72e69d9a
hwan <hshlego@naver.com>
2022-07-14 22:02:18 +0900

    test : add 방에 속한 인원 전부 조회하는 통합 테스트

--- src/test/java/com/schedch/mvp/integrate/ParticipantIntegrateTest.java
+++ src/test/java/com/schedch/mvp/integrate/ParticipantIntegrateTest.java
@@ -1,17 +1,24 @@
 package com.schedch.mvp.integrate;
 
+import com.schedch.mvp.controller.ParticipantController;
+import com.schedch.mvp.dto.ParticipantResponseDto;
 import com.schedch.mvp.model.Participant;
+import com.schedch.mvp.model.Room;
+import com.schedch.mvp.model.RoomDate;
 import com.schedch.mvp.model.Schedule;
 import com.schedch.mvp.repository.ParticipantRepository;
 import com.schedch.mvp.service.ParticipantService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,6 +26,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 public class ParticipantIntegrateTest {
 
+    @Autowired
+    ParticipantController participantController;
     @Autowired
     ParticipantService participantService;
     @Autowired
@@ -51,5 +60,42 @@ public class ParticipantIntegrateTest {
         Participant foundParticipant2 = participantRepository.findById(foundParticipant.getId()).get();
         assertThat(foundParticipant2.getScheduleList().get(0).getStartTime()).isEqualTo(LocalTime.of(9, 30, 0));
 
+    }
+
+    @Test
+    public void 참가자_전원_불러오기() throws Exception {
+        //given
+        Room room = createRoom();
+        Participant participant1 = new Participant("p1", "", false);
+        Participant participant2 = new Participant("p2", "", false);
+        Participant participant3 = new Participant("p3", "", false);
+        room.addParticipant(participant1); room.addParticipant(participant2); room.addParticipant(participant3);
+
+        participant1.addSchedule(new Schedule(LocalDate.of(2022, 2, 2), LocalTime.of(2, 30, 0), LocalTime.of(4, 0, 0)));
+        participant1.addSchedule(new Schedule(LocalDate.of(2022, 2, 2), LocalTime.of(6, 30, 0), LocalTime.of(10, 0, 0)));
+
+        participant2.addSchedule(new Schedule(LocalDate.of(2022, 4, 1), LocalTime.of(2, 30, 0), LocalTime.of(10, 0, 0)));
+
+        em.persist(room);
+
+        //when
+        List<ParticipantResponseDto> allParticipantsInRoom = participantService.findAllParticipantsInRoom(room.getUuid());
+
+        //then
+        assertThat(allParticipantsInRoom.size()).isEqualTo(3);
+    }
+
+    private Room createRoom() {
+        String title = "testTitle";
+        List<RoomDate> roomDates = new ArrayList<>();
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
+
+        LocalTime startTime = LocalTime.of(16, 00);
+        LocalTime endTime   = LocalTime.of(23, 00);
+
+        //when
+        return new Room(title, roomDates, startTime, endTime);
     }
 }
