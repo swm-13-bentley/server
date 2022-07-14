ef62b6c81837466f62a840a45acf7fea407b82c7
hwan <hshlego@naver.com>
2022-07-14 21:07:50 +0900

    feat : create AvailableRequestDto

---
+++ src/main/java/com/schedch/mvp/dto/AvailableRequestDto.java
@@ -0,0 +1,31 @@
+package com.schedch.mvp.dto;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.schedch.mvp.adapter.LocalDateAdapter;
+import com.schedch.mvp.adapter.LocalTimeAdapter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class AvailableRequestDto {
+    private String participantName;
+    private String password;
+    private List<TimeBlockDto> timeBlockDtoList = new ArrayList<>();
+
+    public String toString() {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .registerTypeAdapter(LocalTime.class, new LocalTimeAdapter())
+                .create();
+
+        return gson.toJson(this);
+    }
+}
