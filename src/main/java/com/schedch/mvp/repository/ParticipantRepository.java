2766ce3ad7316d92ed5c3fe5cc617da3dfc648b7
hwan <hshlego@naver.com>
2022-07-14 21:06:05 +0900

    feat : Add findParticipantByParticipantNameAndRoom

--- src/main/java/com/schedch/mvp/repository/ParticipantRepository.java
+++ src/main/java/com/schedch/mvp/repository/ParticipantRepository.java
@@ -1,7 +1,11 @@
 package com.schedch.mvp.repository;
 
 import com.schedch.mvp.model.Participant;
+import com.schedch.mvp.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+    Optional<Participant> findParticipantByParticipantNameAndRoom(String participantName, Room room);
 }
