d98e23485b7d2098b7c929b258faedec3686a31c
hwan <hshlego@naver.com>
2022-07-14 13:47:09 +0900

    feat : modify @ControllerAdvice -> @RestControllerAdvice

--- src/main/java/com/schedch/mvp/controller/handler/ControllerHandler.java
+++ src/main/java/com/schedch/mvp/controller/handler/ControllerHandler.java
@@ -5,10 +5,10 @@ import com.google.gson.JsonObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
