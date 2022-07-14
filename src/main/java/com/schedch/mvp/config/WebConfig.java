914549abbdc6f68082aaabc897b6de552c3d4cbc
hwan <hshlego@naver.com>
2022-07-14 21:03:46 +0900

    feat : Add cors allowing

---
+++ src/main/java/com/schedch/mvp/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.schedch.mvp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS");
+    }
+}
+
+
