1d61793195157d44fd733a83c1ab4c0ce4e510d4
hwan <hshlego@naver.com>
2022-07-22 10:55:47 +0900

    feat: add NoSuchElementHandler 추가됨

--- src/main/java/com/schedch/mvp/controller/handler/ControllerHandler.java
+++ src/main/java/com/schedch/mvp/controller/handler/ControllerHandler.java
@@ -2,23 +2,46 @@ package com.schedch.mvp.controller.handler;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ControllerHandler {
+
+    private final Gson gson;
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult()
                 .getAllErrors()
                 .get(0)
                 .getDefaultMessage();
 
+        JsonObject errorJson = getErrorJson(errorMessage);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(gson.toJson(errorJson));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity handleNoSuchElementException(NoSuchElementException e) {
+        JsonObject errorJson = getErrorJson(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(gson.toJson(errorJson));
+    }
+
+
+    private JsonObject getErrorJson(String errorMessage) {
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("message", errorMessage);
-        return new ResponseEntity<>(new Gson().toJson(jsonObject), HttpStatus.BAD_REQUEST);
+        return jsonObject;
     }
 }
