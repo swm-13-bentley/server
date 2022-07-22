a0d9e8ace22f05b8f814108bc72b54a6c3995cac
hwan <hshlego@naver.com>
2022-07-22 12:10:08 +0900

    feat : modify 에러 캐칭 변경

--- src/main/java/com/schedch/mvp/controller/ParticipantController.java
+++ src/main/java/com/schedch/mvp/controller/ParticipantController.java
@@ -6,14 +6,6 @@ import com.schedch.mvp.dto.AvailableRequestDto;
 import com.schedch.mvp.dto.ParticipantRequestDto;
 import com.schedch.mvp.dto.ParticipantResponseDto;
 import com.schedch.mvp.service.ParticipantService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -21,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,66 +20,33 @@ import java.util.NoSuchElementException;
 public class ParticipantController {
 
     private final ParticipantService participantService;
+    private final Gson gson;
 
     @PostMapping("/room/{roomUuid}/participant/entry")
     public ResponseEntity participantFind(@PathVariable String roomUuid,
-                                          @RequestBody ParticipantRequestDto participantRequestDto) {
+                                          @RequestBody ParticipantRequestDto participantRequestDto) throws IllegalAccessException {
         String participantName = participantRequestDto.getParticipantName();
         String password = participantRequestDto.getPassword();
-        Gson gson = new Gson();
 
-        try {
-            ParticipantResponseDto participantResponseDto
-                    = participantService.findUnSignedParticipantAndValidate(roomUuid, participantName, password);
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(gson.toJson(participantResponseDto));
+        ParticipantResponseDto participantResponseDto
+                = participantService.findUnSignedParticipantAndValidate(roomUuid, participantName, password);
 
-        } catch (IllegalAccessException e1) {//이름 중복
-            JsonObject errorJsonObject = new JsonObject();
-            errorJsonObject.addProperty("message", e1.getMessage());
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(gson.toJson(errorJsonObject));
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(gson.toJson(participantResponseDto));
 
-        } catch (NoSuchElementException e2) {//해당 roomUuid에 대한 방 없음
-            JsonObject errorJsonObject = new JsonObject();
-            errorJsonObject.addProperty("message", e2.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(gson.toJson(errorJsonObject));
-        }
     }
 
     @PostMapping("/room/{roomUuid}/participant/available")
     public ResponseEntity participantAvailablePost(@PathVariable String roomUuid,
                                                    @RequestBody AvailableRequestDto availableRequestDto) {
-        Gson gson = new Gson();
-
-        try {
-            participantService.saveParticipantAvailable(roomUuid, availableRequestDto);
-
-            return ResponseEntity.status(HttpStatus.OK).build();
-
-        } catch (NoSuchElementException e) {//해당 roomUuid에 대한 방 없음
-            JsonObject errorJsonObject = new JsonObject();
-            errorJsonObject.addProperty("message", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(gson.toJson(errorJsonObject));
-        }
+        participantService.saveParticipantAvailable(roomUuid, availableRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/room/{roomUuid}/group")
     public ResponseEntity groupSchedulesFind(@PathVariable String roomUuid) {
-        Gson gson = new Gson();
-
-        try {
-            List<ParticipantResponseDto> participantResponseDtoList = participantService.findAllParticipantsInRoom(roomUuid);
-            return ResponseEntity.status(HttpStatus.OK).body(participantResponseDtoList);
-        }
-        catch (NoSuchElementException e) {
-            JsonObject errorJsonObject = new JsonObject();
-            errorJsonObject.addProperty("message", e.getMessage());
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(gson.toJson(errorJsonObject));
-        }
+        List<ParticipantResponseDto> participantResponseDtoList = participantService.findAllParticipantsInRoom(roomUuid);
+        return ResponseEntity.status(HttpStatus.OK).body(participantResponseDtoList);
     }
 
 }
