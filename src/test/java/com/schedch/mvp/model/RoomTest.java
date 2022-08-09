6aff8a2523b2b735af59d29d63bf7c4487e25f74
hwan <hshlego@naver.com>
2022-08-10 03:16:10 +0900

    test : change 방 생성을 위한 날짜 변경

--- src/test/java/com/schedch/mvp/model/RoomTest.java
+++ src/test/java/com/schedch/mvp/model/RoomTest.java
@@ -29,25 +29,4 @@ class RoomTest {
         //then
         assertThat(room.getUuid()).isNotNull();
     }
-
-    @Test
-    public void 새벽_방_생성() throws Exception{
-        //given
-        String title = "testTitle";
-        List<RoomDate> roomDates = new ArrayList<>();
-        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
-        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 02)));
-        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 03)));
-
-        LocalTime startTime = LocalTime.of(22, 00);
-        LocalTime endTime   = LocalTime.of(04, 00);
-
-        //when
-        Room room = new Room(title, roomDates, startTime, endTime);
-
-        //then
-        assertThat(room.getUuid()).isNotNull();
-        assertThat(room.getRoomDates().size()).isEqualTo(4);
-        assertThat(room.getRoomDates().get(3)).isEqualTo(LocalDate.of(2022, 06, 04));
-    }
 }
