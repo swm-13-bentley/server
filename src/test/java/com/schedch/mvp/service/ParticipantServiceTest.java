d3903571c03dd594bce3f4e72151544343d36385
hwan <hshlego@naver.com>
2022-08-10 06:03:45 +0900

    test : add 새벽 시간 방에 대한 유저 입력 테스트

--- src/test/java/com/schedch/mvp/service/ParticipantServiceTest.java
+++ src/test/java/com/schedch/mvp/service/ParticipantServiceTest.java
@@ -1,5 +1,7 @@
 package com.schedch.mvp.service;
 
+import com.google.gson.Gson;
+import com.schedch.mvp.adapter.TimeAdapter;
 import com.schedch.mvp.dto.ParticipantResponseDto;
 import com.schedch.mvp.dto.TimeBlockDto;
 import com.schedch.mvp.model.Participant;
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -21,14 +24,22 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @WebMvcTest(ParticipantService.class)
+@Import(TimeAdapter.class)
 class ParticipantServiceTest {
 
-    @Autowired ParticipantService participantService;
-    @MockBean ParticipantRepository participantRepository;
-    @MockBean RoomService roomService;
+    @Autowired
+    ParticipantService participantService;
+    @Autowired
+    TimeAdapter timeAdapter;
+    @MockBean
+    ParticipantRepository participantRepository;
+    @MockBean
+    RoomService roomService;
+
 
     String roomUuid = "testRoomUuid";
     String participantName = "testName";
@@ -39,7 +50,6 @@ class ParticipantServiceTest {
         //given
         when(roomService.getRoom(roomUuid))
                 .thenThrow(new NoSuchElementException());
-
 
         //when
         try {
@@ -127,16 +137,62 @@ class ParticipantServiceTest {
                 Arrays.asList()
         );
 
+        LocalTime roomStartTime = LocalTime.of(0, 0);
+
         //when
-        List<Schedule> scheduleList1 = participantService.changeTimeBlockDtoToSchedule(timeBlockDto1);
-        List<Schedule> scheduleList2 = participantService.changeTimeBlockDtoToSchedule(timeBlockDto2);
-        List<Schedule> scheduleList3 = participantService.changeTimeBlockDtoToSchedule(timeBlockDto3);
+        List<Schedule> scheduleList1 = participantService.changeTimeBlockDtoToSchedule(timeBlockDto1, roomStartTime);
+        List<Schedule> scheduleList2 = participantService.changeTimeBlockDtoToSchedule(timeBlockDto2, roomStartTime);
+        List<Schedule> scheduleList3 = participantService.changeTimeBlockDtoToSchedule(timeBlockDto3, roomStartTime);
 
         //then
         assertThat(scheduleList1.size()).isEqualTo(3);
         assertThat(scheduleList1.get(2).getEndTime()).isEqualTo(LocalTime.of(10, 29, 0));
         assertThat(scheduleList2.size()).isEqualTo(1);
         assertThat(scheduleList3.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void 새벽시간_스케쥴_입력_테스트() throws Exception {
+        //given
+        TimeBlockDto timeBlockDto = new TimeBlockDto(
+                LocalDate.of(2022, 4, 1),
+                Arrays.asList(42, 43, 44, 45, 46, 47, 48, 49, 50, 56, 57, 58) //21시 - 1시 29분, 4시 - 5시 29분
+        );
+
+        //방 시작 시간: 8시
+        LocalTime roomStartTime = LocalTime.of(20, 0);
+
+        //when
+        List<Schedule> scheduleList = participantService.changeTimeBlockDtoToSchedule(timeBlockDto, roomStartTime);
+
+        //then
+        assertThat(scheduleList.size()).isEqualTo(2);
+        assertThat(scheduleList.get(0).getStartTime()).isEqualTo(LocalTime.of(21, 0));
+        assertThat(scheduleList.get(0).getEndTime()).isEqualTo(LocalTime.of(1, 29));
+        assertThat(scheduleList.get(1).getStartTime()).isEqualTo(LocalTime.of(4, 0));
+        assertThat(scheduleList.get(1).getEndTime()).isEqualTo(LocalTime.of(5, 29));
+    }
+
+    @Test
+    public void 새벽시간_스케줄_반환_테스트() throws Exception {
+        //given
+        Participant participant = new Participant("some name", "pwd", false);
+
+        LocalTime roomStartTime = LocalTime.of(20, 0); //방 시작 시간: 8시
+        LocalDate availableDate = LocalDate.of(2022, 8, 10);
+        participant.addSchedule(new Schedule(availableDate, LocalTime.of(21, 0), LocalTime.of(21, 59), roomStartTime)); //42 ~ 43, 2
+        participant.addSchedule(new Schedule(availableDate, LocalTime.of(23, 0), LocalTime.of(4, 29), roomStartTime)); //46 ~ 56, 11
+        participant.addSchedule(new Schedule(availableDate, LocalTime.of(8, 0), LocalTime.of(8, 59), roomStartTime)); //64 ~ 65, 2
+
+        //when
+        ParticipantResponseDto res = new ParticipantResponseDto(participant);
+
+        //then
+        assertThat(res.getAvailable().size()).isEqualTo(1);
+
+        List<Integer> availableTimeList = res.getAvailable().get(0).getAvailableTimeList();
+        assertThat(availableTimeList.size()).isEqualTo(15);
+        assertThat(new Gson().toJson(availableTimeList)).isEqualTo("[42,43,46,47,48,49,50,51,52,53,54,55,56,64,65]");
     }
 
     private Room getRoom() {
