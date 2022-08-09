187823383e673caadda1e3ebf318be7b7563b1c6
hwan <hshlego@naver.com>
2022-08-10 02:12:04 +0900

    test : add 새벽 방 생성을 위한 테스트 추가

--- src/test/java/com/schedch/mvp/model/RoomTest.java
+++ src/test/java/com/schedch/mvp/model/RoomTest.java
@@ -17,8 +17,8 @@ class RoomTest {
         String title = "testTitle";
         List<RoomDate> roomDates = new ArrayList<>();
         roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
-        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
-        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 02)));
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 03)));
 
         LocalTime startTime = LocalTime.of(16, 00);
         LocalTime endTime   = LocalTime.of(23, 00);
@@ -28,5 +28,26 @@ class RoomTest {
 
         //then
         assertThat(room.getUuid()).isNotNull();
+    }
+
+    @Test
+    public void 새벽_방_생성() throws Exception{
+        //given
+        String title = "testTitle";
+        List<RoomDate> roomDates = new ArrayList<>();
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 01)));
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 02)));
+        roomDates.add(new RoomDate(LocalDate.of(2022, 06, 03)));
+
+        LocalTime startTime = LocalTime.of(22, 00);
+        LocalTime endTime   = LocalTime.of(04, 00);
+
+        //when
+        Room room = new Room(title, roomDates, startTime, endTime);
+
+        //then
+        assertThat(room.getUuid()).isNotNull();
+        assertThat(room.getRoomDates().size()).isEqualTo(4);
+        assertThat(room.getRoomDates().get(3)).isEqualTo(LocalDate.of(2022, 06, 04));
     }
 }
