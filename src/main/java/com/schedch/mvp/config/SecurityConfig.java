0af1ef2baa439393d3edb638504b0d37485c2bb4
hwan <hshlego@naver.com>
2022-09-01 17:19:15 +0900

    SecurityConfig created

---
+++ src/main/java/com/schedch/mvp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.schedch.mvp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().permitAll())
+                .build();
+    }
+}
