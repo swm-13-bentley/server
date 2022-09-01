1c2ccc41c602cfd974f0e18e257339e49f0fd7cc
hwan <hshlego@naver.com>
2022-09-01 17:18:05 +0900

    GoogleLoginDto created

---
+++ src/main/java/com/schedch/mvp/dto/google/GoogleLoginDto.java
@@ -0,0 +1,31 @@
+package com.schedch.mvp.dto.google;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class GoogleLoginDto {
+    private String iss;
+    private String azp;
+    private String aud;
+    private String sub;
+    private String hd;
+    private String email;
+    private String emailVerified;
+    private String atHash;
+    private String name;
+    private String picture;
+    private String givenName;
+    private String familyName;
+    private String locale;
+    private String iat;
+    private String exp;
+    private String alg;
+    private String kid;
+    private String typ;
+    private String accessToken;
+    private String expiresIn;
+    private String refreshToken;
+    private String scope;
+}
