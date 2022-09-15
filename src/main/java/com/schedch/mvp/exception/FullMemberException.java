05fbf94ac610308c676c61ee9632c4d86ae870c2
hwan <hshlego@naver.com>
2022-09-15 11:52:53 +0900

    custom exception added: Error to throw when member is full

---
+++ src/main/java/com/schedch/mvp/exception/FullMemberException.java
@@ -0,0 +1,13 @@
+package com.schedch.mvp.exception;
+
+public class FullMemberException extends RuntimeException {
+
+    public FullMemberException() {
+
+    }
+
+    public FullMemberException(String message) {
+        super(message);
+    }
+
+}
