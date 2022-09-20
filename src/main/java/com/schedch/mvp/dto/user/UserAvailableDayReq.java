25ef0d3bf42070b5884273d4db2f60a7fe010a29
hwan <hshlego@naver.com>
2022-09-20 19:05:47 +0900

    Initial creation

---
+++ src/main/java/com/schedch/mvp/dto/user/UserAvailableDayReq.java
@@ -0,0 +1,15 @@
+package com.schedch.mvp.dto.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class UserAvailableDayReq {
+
+    private List<LocalDate> availableDates;
+
+}
