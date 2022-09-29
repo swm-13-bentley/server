21c9b7c25cf204d8e35a10994daa4225f61975cb
hwan <hshlego@naver.com>
2022-09-29 10:52:14 +0900

    Initial creation

---
+++ src/main/java/com/schedch/mvp/mapper/UserCalendarMapper.java
@@ -0,0 +1,28 @@
+package com.schedch.mvp.mapper;
+
+import com.schedch.mvp.dto.user.SubCalendarRes;
+import com.schedch.mvp.dto.user.UserCalendarRes;
+import com.schedch.mvp.model.UserCalendar;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring", uses = SubCalendarMapper.class)
+public interface UserCalendarMapper {
+    UserCalendarMapper INSTANCE = Mappers.getMapper(UserCalendarMapper.class);
+
+    @Mapping(target = "subCalendarList", expression = "java(getSubCalendarList(userCalendar))")
+    UserCalendarRes userCalendar2Res(UserCalendar userCalendar);
+
+    default List<SubCalendarRes> getSubCalendarList(UserCalendar userCalendar) {
+        List<SubCalendarRes> subCalendarResList = userCalendar.getSubCalendarList().stream()
+                .map(subCalendar -> SubCalendarMapper.INSTANCE.subCalendar2Res(subCalendar))
+                .collect(Collectors.toList());
+
+        return subCalendarResList;
+    }
+
+}
