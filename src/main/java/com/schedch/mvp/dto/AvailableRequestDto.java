29f76a275759cddb65f0b95f8ca275f6742ec6e7
hwan <hshlego@naver.com>
2022-10-07 14:45:11 +0900

    - password is not needed

--- src/main/java/com/schedch/mvp/dto/AvailableRequestDto.java
+++ src/main/java/com/schedch/mvp/dto/AvailableRequestDto.java
@@ -1,15 +1,8 @@
 package com.schedch.mvp.dto;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.schedch.mvp.adapter.LocalDateAdapter;
-import com.schedch.mvp.adapter.LocalTimeAdapter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +11,6 @@ import java.util.List;
 public class AvailableRequestDto {
 
     private String participantName;
-    private String password;
 
     private List<TimeBlockDto> available = new ArrayList<>();
 }
