cfee1640b66d412d7bb397f9f75538b9712ca8f7
hwan <hshlego@naver.com>
2022-10-25 13:19:33 +0900

    - unused method deleted

--- src/main/java/com/schedch/mvp/repository/ParticipantRepository.java
+++ src/main/java/com/schedch/mvp/repository/ParticipantRepository.java
@@ -11,13 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-    @Query("select p from Participant p" +
-            " where p.participantName = :participantName" +
-            " and p.room = :room" +
-            " and p.isSignedIn = :isSignedIn")
-    Optional<Participant> findParticipantByParticipantNameAndRoomAndIsSignedIn(@Param("participantName") String participantName,
-                                                                               @Param("room") Room room,
-                                                                               @Param("isSignedIn") boolean isSignedIn);
     @Query("select distinct p from Participant p" +
             " left join fetch p.scheduleList" +
             " where p.room = :room" +
