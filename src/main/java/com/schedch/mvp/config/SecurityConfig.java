6b21926ddf851095647e79740977eb339a706ba3
hwan <hshlego@naver.com>
2022-11-15 16:15:08 +0900

    - fixing cors error

--- src/main/java/com/schedch/mvp/config/SecurityConfig.java
+++ src/main/java/com/schedch/mvp/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.schedch.mvp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,6 +34,7 @@ public class SecurityConfig {
                 .apply(new MyCustomDsl()) // 커스텀 필터 등록
                 .and()
                 .authorizeRequests(authorize -> authorize
+                        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .antMatchers("/user/**")
                         .access("hasRole('ROLE_USER')")
                         .anyRequest().permitAll())
